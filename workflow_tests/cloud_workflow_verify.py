1a39312fb8020ce7c02dcff8c0a2a98eb6e04293
Alexander OSTROVSKY <a.ostrovsky@mac.com>
2020-04-02 13:12:51 -0700

    location fixed

--- workflow_tests/cloud_workflow_verify.py
+++ workflow_tests/cloud_workflow_verify.py
@@ -11,25 +11,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--api_key", type=str, help="Galaxy API token")
 parser.add_argument("-g", "--galaxy_url", type=str, help="URL for galaxy instance. On cloudman instance, must use extension as well as domain")
 parser.add_argument("-c", "--comparefile", type=str, help="Data link 1")
+parser.add_argument("-w", "--workflow", type=str, help="workflow file")
 args = parser.parse_args()
 
 api_key = args.api_key
 galaxy_url = args.galaxy_url
 comparefile = args.comparefile
+workflow = args.workflow
 
 #Connect to Galaxy
 gi = GalaxyInstance(url=galaxy_url, key=api_key, verify = False)
 
-#download most recent dataset in history
+#prep to download most recent dataset in history
 history_id = gi.histories.get_current_history()["id"]
-#print(history_id)
 
 attempt_count =  0
 if len(gi.histories.get_current_history()['state_ids']['ok']) == 9 and len(gi.histories.get_current_history()['state_ids']['queued']) == 0:
     data = gi.histories.get_current_history()['state_ids']['ok'][-1]
     #print(data)
     dataset = gi.datasets.show_dataset(history_id, data)
-    gi.datasets.download_dataset(data, file_path = "/Users/alexanderostrovsky/Desktop/Galaxy_work/galaxy-functional-test-script/workflow_tests/outfile.txt", use_default_filename = False)
+    gi.datasets.download_dataset(data, file_path = "./outfile.txt", use_default_filename = False)
     try:
         filecmp.cmp("test.txt", "outfile.txt", shallow = False) == True
     except:
